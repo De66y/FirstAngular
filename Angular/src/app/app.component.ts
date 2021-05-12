f54833141d2aded1d3e91305da3776fa7355008b
De Smits <debbbske@hotmail.com>
2021-05-12 09:17:38 +0200

    Eerste Angular lab

---
+++ Angular/src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Contact } from './models/contact';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
+})
+
+export class AppComponent {
+  title: 'titel';
+  contacts: Contact[] = [
+    { firstName: 'Sam', surname: 'Smith', email: 'sam.smith@music.com' },
+    { firstName: 'Frank', surname: 'Muscles', email: 'frank@muscles.com' },
+    { firstName: 'Eddy', surname: 'Valentino', email: 'eddy@valfam.co.uk' }
+  ];
+  newContact = {} as Contact;
+
+  addContact(): void{
+    this.contacts.push(this.newContact);
+  }
+}
