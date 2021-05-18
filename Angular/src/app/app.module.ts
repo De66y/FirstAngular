21eabc7ab79bb3a6d8e0845f44cee06df76ab19b
De Smits <debbbske@hotmail.com>
2021-05-18 11:21:03 +0200

    Lab 4 tm 3

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import {ContactNamePipe} from "./pipes/contact-name.pipe";
+import {ContactNamePipe} from './pipes/contact-name.pipe';
 
 @NgModule({
   declarations: [
